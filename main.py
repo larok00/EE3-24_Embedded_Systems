b3c48c1f58d757ff8a27ea17077846ed8e511772
Koral Hassan <koraluludag1@hotmail.com>
2018-02-08 20:07:01 +0000

    got main working

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ from umqtt.simple import MQTTClient
 
 import time
 
-def setup_i2c_connection():
+def setup_i2c_connection(i2c):
 	#micropython main programme for I2C connection
 	
 	#the code below works on the screen interface
-	#set the connection
 	
-	i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
 	
 	#this function allows to check if the slave address is still 72
 	i2c.scan()
@@ -24,7 +22,7 @@ def setup_i2c_connection():
 	i2c.writeto(72, bytearray([0x01, 0x44, 0x23]))
 	i2c.writeto(72, bytearray([0x00]))
 
-def read_data():
+def read_data(i2c):
 	data = i2c.readfrom(72, 2)
 	int_data = int.from_bytes(data, 'big')
 	
@@ -35,6 +33,8 @@ def read_data():
 	}
 	
 	payload = dumps(data_record)
+	
+	return payload
 
 def connect_to_network():
 	ap_if = network.WLAN(network.AP_IF)
@@ -51,13 +51,12 @@ def connect_to_network():
 	else:
 		print("Failure")
 
-def connect_client():
+def connect_client(client):
 	topic = "esys/Embedded_girls(and_Koral)/flex"
-
-	client = MQTTClient(unique_id(), "192.168.0.10")
+	
 	client.connect()
 
-def publish():
+def publish(payload, client):
 	topic = "esys/Embedded_girls(and_Koral)/flex"
 	client.publish(topic, bytes(payload, 'utf-8'))
 
@@ -70,13 +69,18 @@ def test_pin():
 
 #################################################################
 
-setup_i2c_connection()
+#set the connection
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+setup_i2c_connection(i2c)
+
 connect_to_network()
-connect_client()
+
+client = MQTTClient(unique_id(), "192.168.0.10")
+connect_client(client)
 
 while (test_pin()==0):
-	read_data()
-	publish()
+	payload = read_data(i2c)
+	publish(payload, client)
 	time.sleep(1)
 
 
