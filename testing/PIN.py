2d47eed248fc7f23787a9c8ad9b95dac43eb4467
Koral Hassan <koraluludag1@hotmail.com>
2018-02-08 20:28:19 +0000

    fixed I/O pins

--- testing/PIN.py
+++
@@ -1,5 +0,0 @@
-from machine import Pin
-
-p14 = Pin(14, Pin.IN, Pin.PULL_UP)
-
-print(p14.value())
--- testing/send_data.py
+++
@@ -1,7 +0,0 @@
-from umqtt.simple import MQTTClient
-
-topic = "esys/Embedded_girls(and_Koral)/flex"
-
-client = MQTTClient(machine.unique_id(), "192.168.0.10")
-
-client.publish(topic, bytes(payload, 'utf-8'))
