ea975bdfa56da340ee1e07d738337b879dba5dad
Koral Hassan <koraluludag1@hotmail.com>
2018-02-15 16:00:57 +0000

    deleted testing files

--- testing/test.py
+++
@@ -1,9 +0,0 @@
-from machine import RTC
-import utime
-
-rtc = RTC()
-now = utime.time()
-tm = utime.localtime(now)
-future_time = utime.mktime((tm[0], tm[1] + 7, tm[2], tm[3], tm[4], tm[5], tm[6], tm[7]))
-
-print(utime.localtime(future_time))
